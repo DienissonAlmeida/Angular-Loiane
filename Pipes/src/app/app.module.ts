9fd0005d0649ab46f92e58d74c840b7bd462fdd3
Dienisson Chinelatto <dienissonchinelatto@gmail.com>
2019-03-06 10:42:18 -0300

    Finalizado módulo pipes.

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData, CommonModule } from '@angular/common';
+import localeDECH from '@angular/common/locales/pt';
 
+registerLocaleData(localeDECH);
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Pipes/src/app/filtro-array.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+describe('FiltroArrayPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroArrayPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
